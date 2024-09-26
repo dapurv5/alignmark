71f155e4261d2d0c2c285fd7cff8ec48ac73e3b0
Apurv Verma <dapurv5@gmail.com>
2024-09-25 21:59:21 -0400

    resolved conflicts

--- plots/plot_reward_diff_with_wm_strength.py
+++ plots/plot_reward_diff_with_wm_strength.py
@@ -87,9 +87,9 @@ def plot_reward_diff(
     watermark_type_to_plot: str = "KGW",
     wm_strength_param_name_to_plot: str = "delta",
 ):
-    data: dict[
-        tuple[str, str], list[tuple[float, list[float], list[float]]]
-    ] = process_files(input_dir, watermark_type_to_plot, wm_strength_param_name_to_plot)
+    data: dict[tuple[str, str], list[tuple[float, list[float], list[float]]]] = (
+        process_files(input_dir, watermark_type_to_plot, wm_strength_param_name_to_plot)
+    )
     with prp.get_context(layout=prp.Layout.ICML, single_col=True) as (
         fig,
         axs,
@@ -107,7 +107,10 @@ def plot_reward_diff(
             reward_diff_stds = []
             for idx, wm_strength in enumerate(wm_strengths):
                 reward_diff_arrays = [
-                    np.array(w) - np.array(u)
+                    (np.array(w) - np.array(u))
+                    / (
+                        np.array(u) + 1e-6
+                    )  # Add a small value to avoid division by zero
                     for w, u in zip(
                         watermarked_rewards[idx], unwatermarked_rewards[idx]
                     )
@@ -115,6 +118,9 @@ def plot_reward_diff(
                 reward_diffs.append(np.mean(reward_diff_arrays))
                 reward_diff_stds.append(np.std(reward_diff_arrays))
             color = next(colors)
+            import pdb
+
+            pdb.set_trace()
             axs.plot(
                 wm_strengths,
                 reward_diffs,
@@ -144,18 +150,15 @@ def plot_reward_diff(
                 elinewidth=0.5,
             )
 
-        axs.set_xlabel(
-            f"Watermark Strength ({wm_strength_param_name_to_plot}) →", fontsize=6
-        )
-        axs.set_ylabel(r"$R_w - R_u$", fontsize=6, labelpad=3)
+        axs.set_xlabel(f"({wm_strength_param_name_to_plot}) →", fontsize=6)
+        axs.set_ylabel(r"$\frac{R_w - R_u}{R_u}$", fontsize=6, labelpad=3)
         # reduce the size of ticks on x and y axis
         axs.tick_params(axis="both", which="major", labelsize=5)
-        axs.set_title(
-            r"Reward score gap with increasing watermark strength", fontsize=6
-        )
+        axs.set_title(r"Relative Reward score gap with increasing temp.", fontsize=6)
         axs.legend(loc="best", fontsize=5)
 
-        axs.set_xlim(left=0, right=1.0)  # Set x-axis to start at 2
+        axs.set_xlim(left=0.2, right=1)  # Set x-axis to start at 2
+        axs.set_ylim(bottom=-5.0, top=1.0)
         axs.grid(True, linestyle="--", alpha=0.7)  # Add grid lines
 
         # plt.tight_layout()
