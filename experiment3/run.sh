8283e2a873ca783eda0e85204aaf899dedf21f8d
Apurv Verma <av787@login02.tartan.njit.edu>
2024-10-29 22:49:15 -0400

    added command for google gemma model

--- experiment3/run.sh
+++ experiment3/run.sh
@@ -6,7 +6,7 @@ set -o errexit -o xtrace -o nounset
 # for seed in $(seq 42 1 42); do
 #     sleep 2
 #     python run_generate.py \
-#         --exp_dir "/project/phan/av787/projs/watermarking/outputs/exp_010_safety" \
+#         --exp_dir "/project/phan/av787/projs/watermarking/outputs/exp_011_safety" \
 #         --model_name "meta-llama/Meta-Llama-3.1-8B-Instruct" \
 #         --dataset_path "/home/av787/safety-data.jsonl" \
 #         --text_field "prompt" \
@@ -15,32 +15,32 @@ set -o errexit -o xtrace -o nounset
 #         --gamma 0.5 \
 #         --threshold 0.05 \
 #         --seed $seed \
-#         --max_gen_len 500 \
+#         --max_gen_len 200 \
 #         --top_p 0.95 \
-#         --batch_size 128
+#         --batch_size 256
 # done
-
-
-for seed in $(seq 42 1 42); do
-    sleep 2
-    python run_generate.py \
-        --exp_dir "/project/phan/av787/projs/watermarking/outputs/exp_010_safety" \
-        --model_name "meta-llama/Meta-Llama-3.1-8B-Instruct" \
-        --dataset_path "/home/av787/safety-data.jsonl" \
-        --text_field "prompt" \
-        --watermark_name "openai" \
-        --threshold 0.05 \
-        --seed $seed \
-        --max_gen_len 500 \
-        --top_p 0.95 \
-        --batch_size 128
-done
 
 
 # for seed in $(seq 42 1 42); do
 #     sleep 2
 #     python run_generate.py \
-#         --exp_dir "/project/phan/av787/projs/watermarking/outputs/exp_010_safety" \
+#         --exp_dir "/project/phan/av787/projs/watermarking/outputs/exp_011_safety" \
+#         --model_name "meta-llama/Meta-Llama-3.1-8B-Instruct" \
+#         --dataset_path "/home/av787/safety-data.jsonl" \
+#         --text_field "prompt" \
+#         --watermark_name "openai" \
+#         --threshold 0.05 \
+#         --seed $seed \
+#         --max_gen_len 200 \
+#         --top_p 0.95 \
+#         --batch_size 256
+# done
+
+
+# for seed in $(seq 42 1 42); do
+#     sleep 2
+#     python run_generate.py \
+#         --exp_dir "/project/phan/av787/projs/watermarking/outputs/exp_011_safety" \
 #         --model_name "mistralai/Mistral-7B-Instruct-v0.3" \
 #         --dataset_path "/home/av787/safety-data.jsonl" \
 #         --text_field "prompt" \
@@ -49,23 +49,57 @@ done
 #         --gamma 0.5 \
 #         --threshold 0.05 \
 #         --seed $seed \
-#         --max_gen_len 500 \
+#         --max_gen_len 200 \
 #         --top_p 0.95 \
-#         --batch_size 128
+#         --batch_size 256
+# done
+
+
+# for seed in $(seq 42 1 42); do
+#     sleep 2
+#     python run_generate.py \
+#         --exp_dir "/project/phan/av787/projs/watermarking/outputs/exp_011_safety" \
+#         --model_name "mistralai/Mistral-7B-Instruct-v0.3" \
+#         --dataset_path "/home/av787/safety-data.jsonl" \
+#         --text_field "prompt" \
+#         --watermark_name "openai" \
+#         --threshold 0.05 \
+#         --seed $seed \
+#         --max_gen_len 200 \
+#         --top_p 0.95 \
+#         --batch_size 256
 # done
 
 
 for seed in $(seq 42 1 42); do
     sleep 2
     python run_generate.py \
-        --exp_dir "/project/phan/av787/projs/watermarking/outputs/exp_010_safety" \
-        --model_name "mistralai/Mistral-7B-Instruct-v0.3" \
+        --exp_dir "/project/phan/av787/projs/watermarking/outputs/exp_011_safety" \
+        --model_name "google/gemma-2-9b-it" \
+        --dataset_path "/home/av787/safety-data.jsonl" \
+        --text_field "prompt" \
+        --watermark_name "maryland" \
+        --delta 2.0 \
+        --gamma 0.5 \
+        --threshold 0.05 \
+        --seed $seed \
+        --max_gen_len 200 \
+        --top_p 0.95 \
+        --batch_size 256
+done
+
+
+for seed in $(seq 42 1 42); do
+    sleep 2
+    python run_generate.py \
+        --exp_dir "/project/phan/av787/projs/watermarking/outputs/exp_011_safety" \
+        --model_name "google/gemma-2-9b-it" \
         --dataset_path "/home/av787/safety-data.jsonl" \
         --text_field "prompt" \
         --watermark_name "openai" \
         --threshold 0.05 \
         --seed $seed \
-        --max_gen_len 500 \
+        --max_gen_len 200 \
         --top_p 0.95 \
-        --batch_size 128
+        --batch_size 256
 done
