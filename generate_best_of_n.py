7572168834a75091e8c3abe72e0515258265c7b5
Apurv Verma <dapurv5@gmail.com>
2024-12-31 00:10:49 -0500

    gen best of n cleanup

--- generate_best_of_n.py
+++ generate_best_of_n.py
@@ -111,13 +111,14 @@ def main(
                     )
                     blob[tgt_score_field] = blob[score_field][top_score_index]
                     for i, src_field in enumerate(src_fields):
+                        tgt_field = tgt_fields[i]
                         assert isinstance(blob[src_field], list)
                         assert (
-                            isinstance(blob[tgt_fields[i]], str)
-                            or isinstance(blob[tgt_fields[i]], float)
-                            or isinstance(blob[tgt_fields[i]], int)
+                            isinstance(blob[tgt_field], str)
+                            or isinstance(blob[tgt_field], float)
+                            or isinstance(blob[tgt_field], int)
                         )
-                        blob[tgt_fields[i]] = blob[src_field][top_score_index]
+                        blob[tgt_field] = blob[src_field][top_score_index]
                     output_fp.write(json.dumps(blob) + "\n")
 
 
