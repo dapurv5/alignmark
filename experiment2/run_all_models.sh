22a20da7063321bf22abd20fcabb42c900447c81
Apurv Verma <av787@njit.edu>
2025-08-11 20:51:58 -0400

    Added names of models

--- experiment2/run_all_models.sh
+++ experiment2/run_all_models.sh
@@ -13,7 +13,9 @@ set -o errexit -o xtrace -o nounset
 # "Qwen/Qwen2.5-0.5B-Instruct" \
 # "Qwen/Qwen2.5-14B-Instruct" )
 
-list_of_models=("Qwen/Qwen2.5-14B-Instruct")
+list_of_models=("Qwen/Qwen2.5-14B-Instruct" \
+	"google/gemma-2-27b" \
+	"mistralai/Mistral-Small-3.1-24B-Instruct-2503" )
 
 
 for model in "${list_of_models[@]}"; do
